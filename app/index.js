59a3d3347a452f6eafd9bd9efa37796097735df2
William Assaly <willassaly@gmail.com>
2024-06-11 23:48:52 -0400

    + Moving pipes

--- app/index.js
+++ app/index.js
@@ -1,6 +1,13 @@
-import React from "react";
+import { useEffect } from "react";
 import { Canvas, useImage, Image } from "@shopify/react-native-skia";
 import { useWindowDimensions } from "react-native";
+import {
+  useSharedValue,
+  withTiming,
+  Easing,
+  withSequence,
+  withRepeat,
+} from "react-native-reanimated";
 const App = () => {
   const { width, height } = useWindowDimensions();
   const bg = useImage(require("../assets/sprites/background-day.png"));
@@ -8,6 +15,22 @@ const App = () => {
   const pipeBottom = useImage(require("../assets/sprites/pipe-green.png"));
   const pipeTop = useImage(require("../assets/sprites/pipe-green-top.png"));
   const base = useImage(require("../assets/sprites/base.png"));
+
+  const x = useSharedValue(width - 50);
+
+  // useEffect(() => {
+  //   x.value = withTiming(0);
+  // }, []);
+  useEffect(() => {
+    x.value = withRepeat(
+      withSequence(
+        withTiming(-150, { duration: 3000, easing: Easing.linear }),
+        withTiming(width, { duration: 0 })
+      ),
+      -1
+    );
+  }, []);
+
   const pipeOffset = 0;
   return (
     <Canvas style={{ width, height }}>
@@ -15,14 +38,14 @@ const App = () => {
       <Image
         image={pipeBottom}
         y={height - 320 + pipeOffset}
-        x={width / 2}
+        x={x}
         width={103}
         height={640}
       />
       <Image
         image={pipeTop}
         y={pipeOffset - 320}
-        x={width / 2}
+        x={x}
         width={103}
         height={640}
       />
